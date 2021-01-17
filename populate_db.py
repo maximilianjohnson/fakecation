36bdb1431c7cae3f25956ba66be1351cb0691a4e
Kevin Yang <kevin.yang015@gmail.com>
2021-01-16 16:47:46 -0800

    updated main.py w/ database searching
    added parsing of database based on coordinates, returning nearest x values as json

--- populate_db.py
+++ populate_db.py
@@ -4,6 +4,7 @@ from fakecation import *
 
 if __name__ == "__main__":
 
+
     df = pd.read_excel("fakecation/static/image_datasets/img_scraped_01162021100023.xls")
 
     with app.app_context():
@@ -11,19 +12,23 @@ if __name__ == "__main__":
 
     c = conn.cursor()
 
+    create_table(conn)
+    
     for index,row in df.iterrows():
 
         lat = row["LAT"]
         lon = row["LNG"]
-        country = row["COUNTRY_ID"]
+        country_id = row["COUNTRY_ID"]
+        country_name = row["COUNTRY_NAME"]
+        city_name = row["CITY_NAME"]
         number = 1
-        gender = 1
+        gender = 0
 
         if type(row["URLS"]) == str:
             urls = row["URLS"].split('\n')
 
             for url in urls:
-                insert_row(conn,lat,lon,country,number,gender,url)
+                insert_row(conn,lat,lon,country_id,country_name,city_name,number,gender,url)
 
     with app.app_context():
         close_db()
