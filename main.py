9a33a1d0bd9a4e2b86f62462fdc7e1a91935d861
brendo-k <brenden.kadota@gmail.com>
2021-01-16 22:28:46 -0800

    changed main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, redirect, session
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
